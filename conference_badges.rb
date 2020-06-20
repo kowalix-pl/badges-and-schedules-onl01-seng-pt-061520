bd4e28f565245f480daee7faff19d6e0c1886066
kowalix007 L.K <kowalix007@gmail.com>
2020-06-20 17:06:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names_new = []
+  names.collect {|name| names_new << badge_maker(name)}
+  names_new
+end 
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |speaker,index|
+  rooms << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+ end
+  rooms
+ 
+end
+
+
+def printer(attendees)
+    assign_rooms(attendees).each do |id| 
+    puts id
+  end 
+    batch_badge_creator(attendees).each do |id|
+    puts id
+ end
+end 
